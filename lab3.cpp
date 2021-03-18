373627ecca356af6d82804e2d8632c217bd02daa
Prucek91 <62542135+Prucek91@users.noreply.github.com>
2021-03-18 21:53:22 +0100

    First commit

---
+++ lab3.cpp
@@ -0,0 +1,36 @@
+// g++ lab3.cpp -pthread -O3 -o lab2 `Magick++-config --cppflags --cxxflags --ldflags --libs`
+#include <stdio.h>
+#include <math.h>
+
+#include <vector>
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <ctime>
+#include <thread>
+
+#include <fstream>
+
+//#include <Magick++.h>
+
+using namespace std;
+//using namespace Magick;
+
+
+/* screen ( integer) coordinate */
+const int iXmax = 800;
+const int iYmax = 800;
+
+
+
+
+
+
+
+int main(int ac, char ** av)
+
+{
+
+
+    return 0;
+}
